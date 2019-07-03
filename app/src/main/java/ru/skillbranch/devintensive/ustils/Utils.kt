2b8469043bf731de1aa42bfe1fb8ea8d32431b9d
Anton <a.k.a.a@bk.ru>
2019-07-03 14:52:59 +0300

    change utils

--- app/src/main/java/ru/skillbranch/devintensive/ustils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ustils/Utils.kt
@@ -1,101 +1,65 @@
 package ru.skillbranch.devintensive.ustils
 
 object Utils {
-    fun parseFullName(fullName:String?):Pair<String?,String?>{
-        if (fullName.isNullOrEmpty()||fullName.trim().isEmpty())
-            return null to null
-        val parts:List<String>? = fullName.trim().split(" ")
-        return when (parts?.size){
-            1-> parts.getOrNull(0) to null
-            else ->{
-                val  firstName =parts?.getOrNull(0)
-                val  lastName =parts?.getOrNull(1)
-                firstName to lastName
-            }
+
+    fun parseFullName(fullName: String?): Pair<String?, String?> {
+        //Удаляем пробелы в начале и в конце
+        var newName = fullName?.trimStart()?.trimEnd()
+        //Заменяем мульти пробелы одним пробелом
+        newName = newName?.replace(Regex("\\s{2,}")," ")
+
+        val parts: List<String>? = newName?.split(" ")
+        var firstName = parts?.getOrNull(0)
+        if (firstName.isNullOrEmpty()) firstName = null
+        var lastName = parts?.getOrNull(1)
+        if (lastName.isNullOrEmpty()) lastName = null
+        return firstName to lastName
+    }
+
+    fun toInitials(firstName: String?, lastName: String?): String? {
+        return when {
+            firstName.isNullOrBlank() && lastName.isNullOrBlank() -> null
+            firstName.isNullOrBlank() -> "${lastName?.get(0)}".toUpperCase()
+            lastName.isNullOrBlank() -> "${firstName[0]}".toUpperCase()
+            else -> "${firstName[0]}${lastName[0]}".toUpperCase()
         }
     }
-    fun toInitials(firstName: String?,lastName: String?):String {
-        val first  = if (firstName.isNullOrEmpty()||firstName.trim().isEmpty()) null else firstName[0].toUpperCase()
-        if (lastName.isNullOrEmpty()) return first.toString()
-        val last:Char = if(lastName.isNullOrEmpty())'\u0000' else{ lastName[0].toUpperCase()}
-        return "$first$last"
-    }
 
 
-    fun transliteration(payload: String, divider:String = " "): String {
-        val mapping = payload.map {
-            when(it){
-                ' '->divider
-                'а'-> 'a'
-                'б' -> 'b'
-                'в' -> 'v'
-                'г' -> 'g'
-                'д' -> 'd'
-                'е' -> 'e'
-                'ё' -> 'e'
-                'ж' -> "zh"
-                'з' -> 'z'
-                'и' -> 'i'
-                'й' -> 'i'
-                'к' -> 'k'
-                'л' -> 'l'
-                'м' -> 'm'
-                'н' -> 'n'
-                'о' -> 'o'
-                'п' -> 'p'
-                'р' -> 'r'
-                'с' -> 's'
-                'т' -> 't'
-                'у' -> 'u'
-                'ф' -> 'f'
-                'х' -> 'h'
-                'ц' -> 'c'
-                'ч' -> "ch"
-                'ш' -> "sh"
-                'щ' -> "sh'"
-                'ъ' -> ""
-                'ы' -> 'i'
-                'ь' -> ""
-                'э' -> 'e'
-                'ю' -> "yu"
-                'я' -> "ya"
-                'А' -> 'A'
-                'Б' -> 'B'
-                'В' -> 'V'
-                'Г' -> 'G'
-                'Д' -> 'D'
-                'Е' -> 'E'
-                'Ё' -> 'E'
-                'Ж' -> "Zh"
-                'З' -> 'Z'
-                'И' -> 'I'
-                'Й' -> 'I'
-                'К' -> 'K'
-                'Л' -> 'L'
-                'М' -> 'M'
-                'Н' -> 'N'
-                'О' -> 'O'
-                'П' -> 'P'
-                'Р' -> 'R'
-                'С' -> 'S'
-                'Т' -> 'T'
-                'У' -> 'U'
-                'Ф' -> 'F'
-                'Х' -> 'H'
-                'Ц' -> 'C'
-                'Ч' -> "Ch"
-                'Ш' -> "Sh"
-                'Щ' -> "Sh'"
-                'Ъ' -> ""
-                'Ы' -> 'I'
-                'Ь' -> ""
-                'Э' -> 'E'
-                'Ю' -> "Yu"
-                'Я' -> "Ya"
-                else -> it
-            }
+    fun transliteration(payload: String, divider: String = " ") = payload.map {
+        val isUpper = it.isUpperCase()
+        val transLetter = when (it.toLowerCase()) {
+            'а' -> "a"
+            'б' -> "b"
+            'в' -> "v"
+            'г' -> "g"
+            'д' -> "d"
+            'е', 'ё', 'э' -> "e"
+            'ж' -> "zh"
+            'з' -> "z"
+            'и', 'й', 'ы' -> "i"
+            'к' -> "k"
+            'л' -> "l"
+            'м' -> "m"
+            'н' -> "n"
+            'о' -> "o"
+            'п' -> "p"
+            'р' -> "r"
+            'с' -> "s"
+            'т' -> "t"
+            'у' -> "u"
+            'ф' -> "f"
+            'х' -> "h"
+            'ц' -> "c"
+            'ч' -> "ch"
+            'ш' -> "sh"
+            'щ' -> "shch"
+            'ъ', 'ь' -> ""
+            'ю' -> "yu"
+            'я' -> "ya"
+            ' ' -> divider
+            else -> "$it"
         }
-
-        return mapping.joinToString(separator = "")
-    }
+        if (isUpper) transLetter.capitalize() else transLetter
+    }.joinToString("")
 }
