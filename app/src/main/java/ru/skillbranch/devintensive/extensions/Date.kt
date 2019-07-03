b910809525f5a3bdc899c34587ee3a4c8d6a1236
Anton <a.k.a.a@bk.ru>
2019-07-03 11:56:58 +0300

    hometask_2 complete

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -0,0 +1,79 @@
+package ru.skillbranch.devintensive.extensions
+
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.abs
+
+const val SECOND = 1000L
+const val MINUTE = 60 * SECOND
+const val HOUR = 60 * MINUTE
+const val DAY = 24 *  HOUR
+
+fun Date.format(pattern:String = "HH:mm:ss dd.MM.yy"):String {
+    val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
+    return dateFormat.format(this)
+}
+
+fun Date.add(value: Int, units:TimeUnits = TimeUnits.SECOND):Date{
+    var time = this.time
+
+    time +=when(units){
+        TimeUnits.SECOND -> value * SECOND
+        TimeUnits.MINUTE -> value * MINUTE
+        TimeUnits.HOUR -> value * HOUR
+        TimeUnits.DAY -> value * DAY
+
+    }
+    this.time = time
+    return this
+}
+
+fun Date.humanizeDiff(date: Date=Date()):String {
+   val raznica = (date.time - this.time)
+    return if (raznica >= 0){
+        when(raznica){
+            in 0L * SECOND..1L * SECOND -> "только что"
+            in 1L * SECOND..45L * SECOND -> "несколько секунд назад"
+            in 45L * SECOND..75L * SECOND -> "минуту назад"
+            in 75L * SECOND..45L * MINUTE -> "${raznica/ MINUTE} ${getNumForm("минуту;минуты;минут", raznica/ MINUTE)} назад"
+            in 45L * MINUTE..75L * MINUTE -> "час назад"
+            in 75L * MINUTE..22L * HOUR -> "${raznica/ HOUR} ${getNumForm("час;часа;часов", raznica/ HOUR)} назад"
+            in 22L * HOUR..26L * HOUR -> "день назад"
+            in 26L * HOUR..360L * DAY -> "${raznica/ DAY} ${getNumForm("день;дня;дней", raznica/ DAY)} назад"
+            else -> "более года назад"
+        }
+    }
+    else{
+        val absraznica = abs(raznica)
+        when (absraznica){
+            in 0L * SECOND..1L * SECOND -> "только что"
+            in 1L * SECOND..45L * SECOND -> "через несколько секунд"
+            in 45L * SECOND..75L * SECOND -> "через минуту"
+            in 75L * SECOND..45L * MINUTE -> "через ${absraznica/ MINUTE} ${getNumForm("минуту;минуты;минут", absraznica/ MINUTE)}"
+            in 45L * MINUTE..75L * MINUTE -> "через час"
+            in 75L * MINUTE..22L * HOUR -> "через ${absraznica/ HOUR} ${getNumForm("час;часа;часов", absraznica/ HOUR)}"
+            in 22L * HOUR..26L * HOUR -> "через день"
+            in 26L * HOUR..360L * DAY -> "через ${absraznica/ DAY} ${getNumForm("день;дня;дней", absraznica/ DAY)}"
+            else -> "более чем через год"
+        }
+    }
+}
+
+private fun getNumForm(pluralForms:String, value: Long):String{
+    val forms = pluralForms.split(";")
+    when (value % 10){
+        1L -> if (value % 100L != 11L)
+            return forms[0]
+        2L, 3L, 4L -> return if (value % 100 !in 12..14)
+            forms[1]
+        else forms[2]
+    }
+    return forms[2]
+}
+
+enum class TimeUnits{
+    SECOND,
+    MINUTE,
+    HOUR,
+    DAY
+}
---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -0,0 +1,105 @@
+package ru.skillbranch.devintensive.utils
+
+object Utils {
+    fun parseFullName(fullName: String?):Pair<String?, String?>{
+
+        val parts:List<String>? = fullName?.split(" ")
+
+        var firstName:String? = null
+        var lastName:String? = null
+
+        if (parts?.getOrNull(0) != "")
+            firstName = parts?.getOrNull(0)
+        if (parts?.getOrNull(1) !="")
+            lastName = parts?.getOrNull(1)
+
+        return Pair(firstName, lastName)
+    }
+
+    fun transliteration(payload: String, divider:String = ""): String {
+        val mapping = payload.map {
+            when(it){
+               ' '->divider
+                'а'-> 'a'
+                'б' -> 'b'
+                'в' -> 'v'
+                'г' -> 'g'
+                'д' -> 'd'
+                'е' -> 'e'
+                'ё' -> 'e'
+                'ж' -> "zh"
+                'з' -> 'z'
+                'и' -> 'i'
+                'й' -> 'i'
+                'к' -> 'k'
+                'л' -> 'l'
+                'м' -> 'm'
+                'н' -> 'n'
+                'о' -> 'o'
+                'п' -> 'p'
+                'р' -> 'r'
+                'с' -> 's'
+                'т' -> 't'
+                'у' -> 'u'
+                'ф' -> 'f'
+                'х' -> 'h'
+                'ц' -> 'c'
+                'ч' -> "ch"
+                'ш' -> "sh"
+                'щ' -> "sh'"
+                'ъ' -> ""
+                'ы' -> 'i'
+                'ь' -> ""
+                'э' -> 'e'
+                'ю' -> "yu"
+                'я' -> "ya"
+                'А' -> 'A'
+                'Б' -> 'B'
+                'В' -> 'V'
+                'Г' -> 'G'
+                'Д' -> 'D'
+                'Е' -> 'E'
+                'Ё' -> 'E'
+                'Ж' -> "Zh"
+                'З' -> 'Z'
+                'И' -> 'I'
+                'Й' -> 'I'
+                'К' -> 'K'
+                'Л' -> 'L'
+                'М' -> 'M'
+                'Н' -> 'N'
+                'О' -> 'O'
+                'П' -> 'P'
+                'Р' -> 'R'
+                'С' -> 'S'
+                'Т' -> 'T'
+                'У' -> 'U'
+                'Ф' -> 'F'
+                'Х' -> 'H'
+                'Ц' -> 'C'
+                'Ч' -> "Ch"
+                'Ш' -> "Sh"
+                'Щ' -> "Sh'"
+                'Ъ' -> ""
+                'Ы' -> 'I'
+                'Ь' -> ""
+                'Э' -> 'E'
+                'Ю' -> "Yu"
+                'Я' -> "Ya"
+                else -> it
+            }
+        }
+
+        return mapping.joinToString(separator = "")
+    }
+
+    fun toInitials(firstName: String?, lastName:String?): String? {
+        var output: String?
+        if ((firstName?.replace(" ", "") == "" && lastName?.replace(" ", "") == "") ||
+            (firstName == null && lastName == null))
+            return null
+        else
+            return "${firstName?.replace(" ", "")?.getOrNull(0) ?: ""} ${lastName?.replace(" ", "")?.getOrNull(0)
+                ?: ""}".toUpperCase()
+    }
+}
