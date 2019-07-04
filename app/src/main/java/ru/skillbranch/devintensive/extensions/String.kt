9a1d44757caa2d44f2aa79430a0c000b6700ffb0
Anton <a.k.a.a@bk.ru>
2019-07-04 14:07:09 +0300

    add new exams

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -0,0 +1,19 @@
+package ru.skillbranch.devintensive.extensions
+
+import android.text.Html
+
+fun String.truncate(length:Int=16):String{
+    //Todo check to count +1
+    var res = this.substring(0,length+1)
+    if (res[res.length-1]==' ')
+        res=res.dropLast(1)
+    return res.plus("...")
+}
+fun String.stripHtml():String{
+    var tags = """(<[^<>]*>)|(  +)"""
+    val regex = Regex(  tags)
+
+    return  regex.replace(this, "") // result: abxcdxef
+
+
+}
